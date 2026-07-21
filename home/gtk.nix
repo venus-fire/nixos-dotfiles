a221147561b03261704024da61db4a99cb61bcc1
venus-fire <venus-fire@tutamail.com>
2026-07-20 21:14:26 -0700

    fix: use gtk.gtk4.extraConfig instead of xdg.configFile
    Remove duplicate settings.ini by using home-manager's built-in
    gtk4.extraConfig option instead of separate xdg.configFile.

--- home/gtk.nix
+++ home/gtk.nix
@@ -13,15 +13,12 @@
       name = "volantes_cursors";
       size = 24;
     };
-  };
 
-  # home-manager's gtk module only sets gtk-theme-name for GTK3, not GTK4.
-  # Explicitly write GTK4 settings so Thunar picks it up.
-  xdg.configFile."gtk-4.0/settings.ini".text = ''
-    [Settings]
-    gtk-theme-name=Adwaita-dark
-    gtk-application-prefer-dark-theme=1
-    gtk-cursor-theme-name=volantes_cursors
-    gtk-cursor-theme-size=24
-  '';
+    # home-manager's gtk module doesn't set gtk-theme-name for GTK4
+    # by default, so configure it explicitly
+    gtk4.extraConfig = {
+      gtk-theme-name = "Adwaita-dark";
+      gtk-application-prefer-dark-theme = 1;
+    };
+  };
 }
